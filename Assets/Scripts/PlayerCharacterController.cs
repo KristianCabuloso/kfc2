6e1ca57aa676f14617673388387bb44357b77309
Sam Cavalheiro <samcava39@gmail.com>
2021-11-12 20:19:44 -0300

    - Movimentação básica; - Alterações no gitignore.

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerCharacterController : MonoBehaviour
+{
+    private CharacterController controller;
+    //Transform cameraTransform;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    [SerializeField] float playerSpeed = 2.0f;
+    [SerializeField] float jumpHeight = 1.0f;
+    [SerializeField] float turnSpeed = 2f;
+    [SerializeField] float minTurnAngle = -90f;
+    [SerializeField] float maxTurnAngle = 90f;
+    private float gravityValue = -9.81f;
+    float rotX;
+
+    private void Start()
+    {
+        //cameraTransform = GetComponentInChildren<Camera>().transform;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // get the mouse inputs
+        float rotY = Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
+        rotX += Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
+
+        // clamp the vertical rotation
+        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+
+        // rotate the camera
+        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + rotY, 0);
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        //Vector3 move = (cameraTransform.right * Input.GetAxis("Horizontal")) + (cameraTransform.forward * Input.GetAxis("Vertical"));
+        Vector3 move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        /*if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }*/
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
