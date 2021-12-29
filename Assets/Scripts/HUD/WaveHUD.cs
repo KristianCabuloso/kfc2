72bcac23871c717710f7d1b60581753aa6b27709
Sam Cavalheiro <samcava39@gmail.com>
2021-12-29 16:49:51 -0300

    Correção de um bug que não sincronizava o número de ondas em todos os PCs (apenas atualizava no servidor)

--- Assets/Scripts/HUD/WaveHUD.cs
+++ Assets/Scripts/HUD/WaveHUD.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Bolt;
 using UnityEngine;
 
 [RequireComponent(typeof(TMPro.TextMeshProUGUI))]
-public class WaveHUD : MonoBehaviour
+public class WaveHUD : GlobalEventListener
 {
     [SerializeField, Tooltip("{current_wave} = Número da onda atual")]
     string textString = "Onda: {current_wave}";
@@ -16,8 +17,13 @@ public class WaveHUD : MonoBehaviour
         text = GetComponent<TMPro.TextMeshProUGUI>();
     }
 
-    public void RefreshTextString(int wave)
+    /*public void RefreshTextString(int wave)
     {
         text.text = textString.Replace("{current_wave}", wave.ToString());
+    }*/
+
+    public override void OnEvent(WaveEvent evnt)
+    {
+        text.text = textString.Replace("{current_wave}", evnt.CurrentWave.ToString());
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,34 +3,41 @@ using System.Collections.Generic;
 using Photon.Bolt;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class Spawner : GlobalEventListener
 {
     public GameObject spawnObject;
     public int spawnObjectLenght = 4;
-    //public GameObject[] spawnObjects;
     public float safeWaitTime = 5f;
-    /*public Vector3 minSpawnOffset = Vector3.zero;
-    public Vector3 maxSpawnOffset = Vector3.one * 1.5f;*/
-    //public bool oneShot = true; // Provavelmente bugará se não for oneShot
 
     BattleManager battleManager;
-    WaveHUD waveHud;
+    //WaveHUD waveHud;
+
+    WaveEvent waveEvent;
     float count;
     int currentWave;
 
     void Start()
     {
+        if (!BoltNetwork.IsServer)
+        {
+            enabled = false;
+            return;
+        }
+
+        waveEvent = WaveEvent.Create();
+        //RefreshAndSendEvent();
+
         battleManager = FindObjectOfType<BattleManager>();
-        waveHud = FindObjectOfType<WaveHUD>();
+        //waveHud = FindObjectOfType<WaveHUD>();
 
         count = safeWaitTime;
-
-        if (!BoltNetwork.IsServer)
-            enabled = false;
     }
 
     void Update()
-    { 
+    {
+        /*if (!BoltNetwork.IsRunning)
+            return;*/
+
         if (battleManager.NPCs.Count == 0)//if (count <= 0f)
         {
             count -= BoltNetwork.FrameDeltaTime;
@@ -38,34 +45,33 @@ public class Spawner : MonoBehaviour
                 return;
                 
             currentWave++;
-            waveHud.RefreshTextString(currentWave);
-
+            RefreshAndSendEvent();
+            //waveHud.RefreshTextString(currentWave);
+            
             for (int i = 0; i < spawnObjectLenght * battleManager.players.Count * currentWave; i++)
             {
-                /*Vector3 spawnPosition = transform.position +
-                                        new Vector3(Random.Range(minSpawnOffset.x, maxSpawnOffset.x),
-                                                    Random.Range(minSpawnOffset.y, maxSpawnOffset.y),
-                                                    Random.Range(minSpawnOffset.z, maxSpawnOffset.z));*/
-
-
                 GameObject spawned = BoltNetwork.Instantiate(spawnObject, transform.position + (Vector3.left * 3.5f * i), Quaternion.identity);
                 EnemyCharacterController enemy = spawned.GetComponent<EnemyCharacterController>();
                 if (enemy)
                     enemy.baseTransform = transform;
             }
 
-            /*foreach (GameObject go in spawnObjects)
-            {
-                GameObject spawned = BoltNetwork.Instantiate(go, transform.position, Quaternion.identity);
-                EnemyCharacterController enemy = spawned.GetComponent<EnemyCharacterController>();
-                if (enemy)
-                    enemy.baseTransform = transform;
-            }*/
-
             count = safeWaitTime;
+        }
+    }
 
-            /*if (oneShot)
-                enabled = false;*/
+    void RefreshAndSendEvent()
+    {
+        waveEvent.CurrentWave = currentWave;
+        waveEvent.Send();
+    }
+
+    public override void EntityAttached(BoltEntity entity)
+    {
+        if (enabled && !entity.IsOwner)
+        {
+            print("TESTEEEE");
+            waveEvent.Send();
         }
     }
 }
