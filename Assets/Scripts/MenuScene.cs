498e679a9eb21a6ac78e756e527ca4e866f4eb79
sam-cavalheiro <samcava39@gmail.com>
2022-02-06 21:29:58 -0300

    - Alteração como o menu principal funciona - Agora mescla o jeito novo (inserindo nome) com o jeito antigo (dando opções de iniciar servidor e cliente); - Ajustes na vida do inimigo; - Ajustes no cooldown de tiro da pistola;

--- Assets/Scripts/MenuScene.cs
+++ Assets/Scripts/MenuScene.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(NetworkMenuScene))]
 public class MenuScene : MonoBehaviour
 {
     public TMPro.TMP_InputField inputField;
     public GameObject mainHolder;
+    public GameObject gameOptionsHolder;
     public GameObject waitHolder;
 
     public void Command_Send()
@@ -16,22 +16,34 @@ public class MenuScene : MonoBehaviour
         {
             PlayerAnalytics.playerName = _playerName;
 
-            if (_playerName == "server")
+            /*if (_playerName == "server")
             {
                 GetComponent<NetworkMenuScene>().Command_StartServer();
             }
             else
             {
                 GetComponent<NetworkMenuScene>().Command_StartClient();
-            }
+            }*/
 
             mainHolder.SetActive(false);
-            waitHolder.SetActive(true);
+            gameOptionsHolder.SetActive(true);
         }
     }
 
     public void Command_Quit()
     {
         Application.Quit();
+    }
+
+    public void Command_Back()
+    {
+        mainHolder.SetActive(true);
+        gameOptionsHolder.SetActive(false);
+    }
+
+    public void Command_StartGame()
+    {
+        gameOptionsHolder.SetActive(false);
+        waitHolder.SetActive(true);
     }
 }
