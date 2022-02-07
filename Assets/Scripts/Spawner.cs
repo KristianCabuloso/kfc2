06d8d55b2bb9f1164053a9758fc23b38805a6167
sam-cavalheiro <samcava39@gmail.com>
2022-02-07 17:35:31 -0300

    Mudanças na forma de spawnar

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,8 @@ public class Spawner : GlobalEventListener
 {
     public GameObject spawnObject;
     public int spawnObjectLenght = 4;
+    public int spawnAmountPerInterval = 3;
+    public float spawnWaitTimeAfterInterval = 3f;
     public float safeWaitTime = 5f;
 
     BattleManager battleManager;
@@ -15,6 +17,7 @@ public class Spawner : GlobalEventListener
     WaveEvent waveEvent;
     float count;
     int currentWave;
+    bool isSpawning;
 
     void Start()
     {
@@ -38,26 +41,45 @@ public class Spawner : GlobalEventListener
         /*if (!BoltNetwork.IsRunning)
             return;*/
 
-        if (battleManager.NPCs.Count == 0)//if (count <= 0f)
+        if (!isSpawning && battleManager.NPCs.Count == 0)//if (count <= 0f)
         {
             count -= BoltNetwork.FrameDeltaTime;
             if (count > 0f)
                 return;
-                
-            currentWave++;
-            RefreshAndSendEvent();
-            //waveHud.RefreshTextString(currentWave);
 
-            for (int i = 0; i < spawnObjectLenght * (battleManager.players.Count + 1) * currentWave; i++)
-            {
-                GameObject spawned = BoltNetwork.Instantiate(spawnObject, transform.position + (Vector3.left * 3.5f * i), Quaternion.identity);
-                EnemyCharacterController enemy = spawned.GetComponent<EnemyCharacterController>();
-                if (enemy)
-                    enemy.baseTransform = transform;
-            }
-
-            count = safeWaitTime;
+            StartCoroutine(Spawn());
         }
+    }
+
+    IEnumerator Spawn()
+    {
+        isSpawning = true;
+
+        currentWave++;
+        RefreshAndSendEvent();
+        //waveHud.RefreshTextString(currentWave);
+
+        int spawnAmount = 0;
+
+        for (int i = 0; i < spawnObjectLenght * (battleManager.players.Count + 1) * currentWave; i++)
+        {
+            spawnAmount++;
+
+            GameObject spawned = BoltNetwork.Instantiate(spawnObject, transform.position + (Vector3.left * 3.5f * spawnAmount), Quaternion.identity);
+            EnemyCharacterController enemy = spawned.GetComponent<EnemyCharacterController>();
+            if (enemy)
+                enemy.baseTransform = transform;
+
+            if (spawnAmount == spawnAmountPerInterval)
+            {
+                spawnAmount = 0;
+                yield return new WaitForSeconds(spawnWaitTimeAfterInterval);
+            }
+        }
+
+        count = safeWaitTime;
+
+        isSpawning = false;
     }
 
     void RefreshAndSendEvent()
